64d9452e402d5e6871bd25b03b22acc8ca4348b3
Yash Jain <benyash04@gmail.com>
2023-02-14 18:54:50 +0530

    feat: added incident structs for tables

--- backend/types/incidents.go
+++ backend/types/incidents.go
@@ -1,10 +1,32 @@
 package types
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Incident struct {
+	gorm.Model
+	Uuid               uuid.UUID
+	Description        string
+	Url                string
+	IncidentCreatedAt  time.Time
+	ProviderIncidentId string
 }
 
 type IncidentUpdate struct {
+	gorm.Model
+	Uuid        uuid.UUID
+	IncidentId  string
+	Description string
+	Status      string
+	StatusTime  string
 }
 
 type IncidentUpdatesComponent struct {
+	gorm.Model
+	IncidentUpdateId string
+	ComponentId      string
 }
