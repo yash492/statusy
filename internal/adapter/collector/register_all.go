d84600febe7d7e4790f0bad27d5b55a0a4952aa9
Yash Jain <benyash04@gmail.com>
2026-02-28 16:04:02 +0530

    feat: add plivo

--- internal/adapter/collector/register_all.go
+++ internal/adapter/collector/register_all.go
@@ -2,10 +2,12 @@ package collector
 
 import (
 	"github.com/yash492/statusy/internal/adapter/collector/atlassian/circleci"
+	"github.com/yash492/statusy/internal/adapter/collector/atlassian/plivo"
 	"github.com/yash492/statusy/internal/adapter/collector/registry"
 )
 
 func RegisterAll() map[string]registry.ProviderBuilderFunc {
 	circleci.Register()
+	plivo.Register()
 	return registry.GetAll()
 }
